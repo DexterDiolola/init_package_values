85c0ded896b9b265737af55fa084a847d2d46518
DexterDiolola <dexterdiolola@gmail.com>
2018-12-20 20:26:48 +0800

    Update init_package_values.py

--- init_package_values.py
+++ init_package_values.py
@@ -14,26 +14,114 @@ class PackageValue:
 			split = dict_param[i]['packages'].split(',')
 
 			if len(split) < 17:
-				split = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+				obj = {}
+				obj['mac'] = dict_param[i]['mac']
+				obj['wallet'] = int(dict_param[i]['wallet'])
+				obj['xxxMinutes'] = -1
+				obj['iHour'] = -1
+				obj['iiHours'] = -1
+				obj['iiiHours'] = -1
+				obj['vHours'] = -1
+				obj['iDay'] = -1
+				obj['iiDays'] = -1
+				obj['ivDays'] = -1
+				obj['iWeek'] = -1
+
+				obj['iiiDays'] = -1
+				obj['viiDays'] = -1
+				obj['xvDays'] = -1
+				obj['xxxDays'] = -1
+
+				obj['dateCreated'] = dict_param[i]['dateCreated']
+				
+				arr.append(obj)
 
 			elif len(split) == 17:	
-				split[7:7] = ['iiiHours', 0]	# splice the array in 7th index of list
+				split[19:] = ['iiiHours', 0, 'iiiDays', 0, 'viiDays', 0, 'xvDays', 0, 'xxxDays', 0]
 
-			obj = {}
-			obj['mac'] = dict_param[i]['mac']
-			obj['wallet'] = int(dict_param[i]['wallet'])
-			obj['xxxMinutes'] = int(split[2])
-			obj['iHour'] = int(split[4])
-			obj['iiHours'] = int(split[6])
-			obj['iiiHours'] = int(split[8])
-			obj['vHours'] = int(split[10])
-			obj['iDay'] = int(split[12])
-			obj['iiDays'] = int(split[14])
-			obj['ivDays'] = int(split[16])
-			obj['iWeek'] = int(split[18])
-			obj['dateCreated'] = dict_param[i]['dateCreated']
+				# sample: ,30Minutes,4,1Hour,20,2Hours,1,5Hours,2,1Day,1,2Days,0,3days,0,1Week,1
+				obj = {}
+				obj['mac'] = dict_param[i]['mac']
+				obj['wallet'] = int(dict_param[i]['wallet'])
+				obj['xxxMinutes'] = int(split[2])
+				obj['iHour'] = int(split[4])
+				obj['iiHours'] = int(split[6])
+				obj['vHours'] = int(split[8])
+				obj['iDay'] = int(split[10])
+				obj['iiDays'] = int(split[12])
+
+				if split[13] == '3days':	
+					obj['iiiDays'] = int(split[14])
+					obj['ivDays'] = 0
+				else:
+					obj['iiiDays'] = 0	
+					obj['ivDays'] = int(split[14])
+
+				obj['iWeek'] = int(split[16])
+
+				obj['iiiHours'] = int(split[18])
+				obj['viiDays'] = int(split[22])
+				obj['xvDays'] = int(split[24])
+				obj['xxxDays'] = int(split[26])
+
+				obj['dateCreated'] = dict_param[i]['dateCreated']
+				
+				arr.append(obj)
+
+			elif len(split) == 19:
+				split[19:] = ['iiiDays', 0, 'viiDays', 0, 'xvDays', 0, 'xxxDays', 0]
+
+				# sample: ,30Minutes,5,1Hour,157,2Hours,106,3Hours,35,5Hours,48,1Day,3,2Days,2,4days,1,1Week,1
+				obj = {}
+				obj['mac'] = dict_param[i]['mac']
+				obj['wallet'] = int(dict_param[i]['wallet'])
+				obj['xxxMinutes'] = int(split[2])
+				obj['iHour'] = int(split[4])
+				obj['iiHours'] = int(split[6])
+				obj['iiiHours'] = int(split[8])
+				obj['vHours'] = int(split[10])
+				obj['iDay'] = int(split[12])
+				obj['iiDays'] = int(split[14])
+				obj['ivDays'] = int(split[16])
+				obj['iWeek'] = int(split[18])
+
+				obj['iiiDays'] = int(split[20])
+				obj['viiDays'] = int(split[22])
+				obj['xvDays'] = int(split[24])
+				obj['xxxDays'] = int(split[26])
+
+				obj['dateCreated'] = dict_param[i]['dateCreated']
+				
+				arr.append(obj)
+
+
+			elif len(split) == 27:
+				# sample: ,30Minutes, 0, 1Hour, 1, 2Hours, 0, 5Hours, 0, 1Day, 1, 2Days, 0, 3days, 0, 1Week, 0, 3Hours, 0, 3Days, 0, 7Days, 0, 15Days, 1, 30Days, 0
+				obj = {}
+				obj['mac'] = dict_param[i]['mac']
+				obj['wallet'] = int(dict_param[i]['wallet'])
+				obj['xxxMinutes'] = int(split[2])
+				obj['iHour'] = int(split[4])
+				obj['iiHours'] = int(split[6])
+				obj['vHours'] = int(split[8])
+				obj['iDay'] = int(split[10])
+				obj['iiDays'] = int(split[12])
+				obj['ivDays'] = 0 # There is no 4Days to any split which length is 27
+				obj['iWeek'] = int(split[16])
+
+				obj['iiiHours'] = int(split[18])
+				obj['iiiDays'] = int(split[20])
+				obj['viiDays'] = int(split[22])
+				obj['xvDays'] = int(split[24])
+				obj['xxxDays'] = int(split[26])
+				
+				obj['dateCreated'] = dict_param[i]['dateCreated']
+				
+				arr.append(obj)
+				
+
+
 			
-			arr.append(obj)
 
 		# print(arr)
 
@@ -71,6 +159,12 @@ class PackageValue:
 			iiDays = subtractor(dict_param[length-i-1]['iiDays'], dict_param[z]['iiDays'])
 			ivDays = subtractor(dict_param[length-i-1]['ivDays'], dict_param[z]['ivDays'])
 			iWeek = subtractor(dict_param[length-i-1]['iWeek'], dict_param[z]['iWeek'])
+
+			iiiDays = subtractor(dict_param[length-i-1]['iiiDays'], dict_param[z]['iiiDays'])
+			viiDays = subtractor(dict_param[length-i-1]['viiDays'], dict_param[z]['viiDays'])
+			xvDays = subtractor(dict_param[length-i-1]['xvDays'], dict_param[z]['xvDays'])
+			xxxDays = subtractor(dict_param[length-i-1]['xxxDays'], dict_param[z]['xxxDays'])
+
 			wallet = dict_param[length-i-1]['wallet']
 
 			if xxxMinutes < 0:	xxxMinutes = 0
@@ -82,6 +176,12 @@ class PackageValue:
 			if iiDays < 0:	iiDays = 0
 			if ivDays < 0:	ivDays = 0
 			if iWeek < 0:	iWeek = 0
+
+			if iiiDays < 0: iiiDays = 0
+			if viiDays < 0: viiDays = 0
+			if xvDays < 0: xvDays = 0
+			if xxxDays < 0: xxxDays = 0
+
 			if wallet < 0:	wallet = 0
 
 
@@ -98,11 +198,17 @@ class PackageValue:
 			obj['iiDays'] = iiDays
 			obj['ivDays'] = ivDays
 			obj['iWeek'] = iWeek
+
+			obj['iiiDays'] = iiiDays
+			obj['viiDays'] = viiDays
+			obj['xvDays'] = xvDays
+			obj['xxxDays'] = xxxDays
+
 			obj['dateCreated'] = dict_param[length-i-1]['dateCreated']
 
 			arr.append(obj)
 
-		#print(arr)
+		# print(arr)
 
 		return arr
 
@@ -161,7 +267,7 @@ class PackageValue:
 				arr2.append(arr[i][j])
 
 		with open('mydata.json', 'w') as f:
-			json.dump(arr2, f, ensure_ascii = False)
+			json.dump('Insertion Complete', f, ensure_ascii = False)
 
 
 		return arr2
@@ -192,9 +298,32 @@ class PackageValue:
 			iiDays = values[i]['iiDays']
 			ivDays = values[i]['ivDays']
 			iWeek = values[i]['iWeek']
+
+			iiiDays = values[i]['iiiDays']
+			viiDays = values[i]['viiDays']
+			xvDays = values[i]['xvDays']
+			xxxDays = values[i]['xxxDays']
+
 			dateCreated = values[i]['dateCreated']
 
-			cursor.callproc('fill_computed_packages', [mac, wallet, xxxMinutes, iHour, iiHours, iiiHours, vHours, iDay, iiDays, ivDays, iWeek, dateCreated])
+			cursor.callproc('fill_computed_packages', [
+				mac, 
+				wallet, 
+				xxxMinutes, 
+				iHour, 
+				iiHours, 
+				iiiHours, 
+				vHours, 
+				iDay, 
+				iiDays, 
+				ivDays, 
+				iWeek, 
+				iiiDays, 
+				viiDays, 
+				xvDays, 
+				xxxDays, 
+				dateCreated])
+			
 			conn.commit()
 
 		# Check for time when insertion is finished
@@ -251,8 +380,6 @@ class PackageValue:
 if __name__ == '__main__':
 	x = PackageValue()
 	xx = x.fill_computed_packages()
-	#xx = x.zzz()
-
 	
 
 
@@ -271,15 +398,23 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-
-
-
 	'''
-	arr = [
+	arr_17 = [
+		{
+			'mac' : 'aaa',
+			'wallet' : 1000,
+			'packages' : ',30Minutes,146,1Hour,178,2Hours,17,5Hours,11,1Day,5,2Days,0,3days,0,1Week,0',
+			'dateCreated' : '2018-11-20'
+		},
+		{
+			'mac' : 'aaa',
+			'wallet' : 2000,
+			'packages' : ',30Minutes,115,1Hour,189,2Hours,123,5Hours,111,1Day,21,2Days,21,3days,21,1Week,65',
+			'dateCreated' : '2018-11-21'
+		}
+	]
+
+	arr_19 = [
 		{
 			'mac' : 'aaa',
 			'wallet' : 1000,
@@ -293,11 +428,68 @@ if __name__ == '__main__':
 			'dateCreated' : '2018-11-21'
 		}
 	]
-	'''
-	# decoded_packages = x.package_decoder(arr)
-	# computed_packages = x.compute_packages(decoded_packages)
 
-	# z = x.xxx()
+	arr_27 = [
+		{
+			'mac' : 'aaa',
+			'wallet' : 1000,
+			'packages' : ',30Minutes,146,1Hour,178,2Hours,17,5Hours,11,1Day,5,2Days,0,3days,0,1Week,0,3Hours,9,3Days,9,7Days,9,15Days,9,30Days,9',
+			'dateCreated' : '2018-11-20'
+		},
+		{
+			'mac' : 'aaa',
+			'wallet' : 2000,
+			'packages' : ',30Minutes,115,1Hour,189,2Hours,123,5Hours,111,1Day,21,2Days,21,3days,21,1Week,65,3Hours,12,3Days,12,7Days,12,15Days,12,30Days,12',
+			'dateCreated' : '2018-11-21'
+		}
+	]
+
+	arr_mix = [
+		{
+			'mac' : 'aaa',
+			'wallet' : 1000,
+			'packages' : ',30Minutes,146,1Hour,178,2Hours,17,5Hours,11,1Day,5,2Days,0,3days,0,1Week,0,3Hours,9,3Days,9,7Days,9,15Days,9,30Days,9',
+			'dateCreated' : '2018-11-20'
+		},
+		{
+			'mac' : 'aaa',
+			'wallet' : 2000,
+			'packages' : ',30Minutes,115,1Hour,189,2Hours,123,5Hours,111,1Day,21,2Days,21,3days,21,1Week,65,3Hours,12,3Days,12,7Days,12,15Days,12,30Days,12',
+			'dateCreated' : '2018-11-21'
+		},
+		{
+			'mac' : 'aaa',
+			'wallet' : 2000,
+			'packages' : ',30Minutes,115,1Hour,189,2Hours,123,5Hours,111,1Day,21,2Days,21,3days,21,1Week,65',
+			'dateCreated' : '2018-11-22'
+		},
+		{
+			'mac' : 'aaa',
+			'wallet' : 2000,
+			'packages' : ',30Minutes,115,1Hour,189,2Hours,123,5Hours,111,1Day,21,2Days,21,4days,21,1Week,65',
+			'dateCreated' : '2018-11-23'
+		},
+		{
+			'mac' : 'aaa',
+			'wallet' : 2000,
+			'packages' : '0',
+			'dateCreated' : '2018-11-24'
+		}
+	]
+
+	xx = x.package_decoder(arr_mix)
+	yy = x.compute_packages(xx)
+
+	with open('test.json', 'w') as f:
+		json.dump(yy, f, ensure_ascii = False)
+	
+	'''
+
+
+
+
+
+
 
 
 
